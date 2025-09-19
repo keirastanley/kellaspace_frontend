57b2a58614d8b29c31008ac70c8bb05564a9312a
keirastanley <keiraestanley@gmail.com>
2025-09-20 00:58:47 +0100

    feat: add slider and basic styling to checkboxes

--- kellaspace-frontend-app/src/components/RecommendationWidget/FilterByTypeCheckboxGroup.tsx
+++ kellaspace-frontend-app/src/components/RecommendationWidget/FilterByTypeCheckboxGroup.tsx
@@ -1,5 +1,9 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import { RecommendationFilter } from "../../interfaces/recommendationFilters";
 import { MediaType } from "../../interfaces/recommendations";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./slider-styles.css";
 
 export const FilterByTypeCheckboxGroup = ({
   selectedFilters,
@@ -13,45 +17,82 @@ export const FilterByTypeCheckboxGroup = ({
   mediaTypes: MediaType[];
 }) => {
   const filters: RecommendationFilter[] = ["All", ...mediaTypes];
+
   return (
-    <div role="checkboxgroup">
-      {filters.map((filter) => (
-        <label key={filter} style={{ display: "block", marginBottom: 4 }}>
-          <input
-            type="checkbox"
-            name={filter}
-            value={filter}
-            checked={selectedFilters?.includes(filter)}
-            onChange={(e) => {
-              const filter = e.target.value as RecommendationFilter;
-              setSelectedFilters((prevFilters) => {
-                if (filter === "All") {
-                  return filters;
-                }
+    <div
+      role="checkboxgroup"
+      css={css`
+        input[type="checkbox"] {
+          display: none;
+        }
+      `}
+    >
+      <Swiper spaceBetween={10} slidesPerView="auto" id="filter-slider">
+        {filters.map((filter, i) => {
+          const checked = selectedFilters?.includes(filter);
 
-                const filteredByAll = prevFilters.includes("All");
+          return (
+            <SwiperSlide key={filter} id={`filter-slide-${i}`}>
+              <label
+                key={filter}
+                css={css`
+                  display: inline-block;
+                  padding: 5px 10px;
+                  border: 1px solid black;
+                  border-radius: 15px;
+                  background-color: white;
+                  color: black;
+                  cursor: pointer;
+                  user-select: none;
+                  transition: all 0.3s ease;
+                  font-size: 12px;
+                  text-align: center;
+                  ${checked &&
+                  css`
+                    background-color: grey;
+                    color: white;
+                  `}
+                `}
+              >
+                <input
+                  type="checkbox"
+                  name={filter}
+                  value={filter}
+                  checked={checked}
+                  onChange={(e) => {
+                    const filter = e.target.value as RecommendationFilter;
+                    setSelectedFilters((prevFilters) => {
+                      if (filter === "All") {
+                        return filters;
+                      }
 
-                if (prevFilters.includes(filter)) {
-                  const indexOfFilterToRemove = prevFilters.indexOf(filter);
-                  return [
-                    ...prevFilters.slice(
-                      filteredByAll ? 1 : 0,
-                      indexOfFilterToRemove
-                    ),
-                    ...prevFilters.slice(indexOfFilterToRemove + 1),
-                  ];
-                }
+                      const filteredByAll = prevFilters.includes("All");
 
-                return [
-                  ...(filteredByAll ? prevFilters.slice(1) : prevFilters),
-                  filter,
-                ];
-              });
-            }}
-          />
-          {filter}
-        </label>
-      ))}
+                      if (prevFilters.includes(filter)) {
+                        const indexOfFilterToRemove =
+                          prevFilters.indexOf(filter);
+                        return [
+                          ...prevFilters.slice(
+                            filteredByAll ? 1 : 0,
+                            indexOfFilterToRemove
+                          ),
+                          ...prevFilters.slice(indexOfFilterToRemove + 1),
+                        ];
+                      }
+
+                      return [
+                        ...(filteredByAll ? prevFilters.slice(1) : prevFilters),
+                        filter,
+                      ];
+                    });
+                  }}
+                />
+                {filter}
+              </label>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </div>
   );
 };
