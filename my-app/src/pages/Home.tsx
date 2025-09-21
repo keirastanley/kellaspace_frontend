bc821d0e7227c81629380d221c5bc3a0ceb8bd26
keirastanley <keiraestanley@gmail.com>
2025-09-21 14:10:59 +0100

    fix: fix dependency array in home page useMemo

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -29,7 +29,7 @@ export const Home = () => {
       );
     }
     return recommendationsSortedByDate.slice(6);
-  }, [selectedFilters]);
+  }, [recommendationsSortedByDate, selectedFilters]);
 
   return (
     <div
