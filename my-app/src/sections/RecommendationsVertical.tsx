7aed5b6b206caff46dac064c422e9eb5ec47c66c
keirastanley <keiraestanley@gmail.com>
2025-09-30 01:32:13 +0100

    feat: create RecommendationsVertical component

---
+++ my-app/src/sections/RecommendationsVertical.tsx
@@ -0,0 +1,75 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { useState } from "react";
+import { FilterByTypeCheckboxGroup } from "../components/FilterByTypeCheckboxGroup";
+import {
+  MediaType,
+  Recommendation,
+  RecommendationWidgetVariant,
+} from "../interfaces";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Mousewheel, FreeMode } from "swiper/modules";
+import { RecommendationWidget } from "../components/RecommendationWidget/RecommendationWidget";
+import SwiperCore from "swiper";
+
+export const RecommendationsVertical = ({
+  recommendations,
+}: {
+  recommendations: Recommendation[];
+}) => {
+  const [selectedFilters, setSelectedFilters] = useState<MediaType[]>([]);
+  const [swiperInstance, setSwiperInstance] = useState<SwiperCore>();
+
+  const mediaTypes = Array.from(
+    new Set(recommendations.map(({ mediaType }) => mediaType))
+  );
+
+  return (
+    <div
+      css={css`
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+      `}
+    >
+      {mediaTypes.length > 1 && (
+        <FilterByTypeCheckboxGroup
+          mediaTypes={mediaTypes}
+          selectedFilters={selectedFilters}
+          setSelectedFilters={setSelectedFilters}
+        />
+      )}
+
+      <Swiper
+        direction="vertical"
+        slidesPerView="auto"
+        spaceBetween={5}
+        watchOverflow={true}
+        onSwiper={setSwiperInstance}
+        mousewheel={{ sensitivity: 1 }}
+        freeMode={true}
+        modules={[Mousewheel, FreeMode]}
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        {recommendations.map((recommendation, i) => (
+          <SwiperSlide
+            key={recommendation.title + recommendation.dateAdded}
+            style={{ height: "100px", width: "100%" }}
+          >
+            <RecommendationWidget
+              recommendation={recommendation}
+              variant={RecommendationWidgetVariant.Expand}
+              onClick={() => {
+                swiperInstance?.slideTo(i);
+              }}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
