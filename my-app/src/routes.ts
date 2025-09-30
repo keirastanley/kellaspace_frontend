08602742b071936cd96db104d440623cef64c487
keirastanley <keiraestanley@gmail.com>
2025-09-30 01:38:18 +0100

    feat: remove demo lists

--- my-app/src/routes.ts
+++ my-app/src/routes.ts
@@ -5,25 +5,6 @@ export const router = createBrowserRouter([
   {
     path: "/",
     Component: Home,
-    children: [
-      { index: true, Component: Home },
-      // { path: "about", Component: About },
-      // {
-      //   path: "auth",
-      //   Component: AuthLayout,
-      //   children: [
-      //     { path: "login", Component: Login },
-      //     { path: "register", Component: Register },
-      //   ],
-      // },
-      // {
-      //   path: "concerts",
-      //   children: [
-      //     { index: true, Component: ConcertsHome },
-      //     { path: ":city", Component: ConcertsCity },
-      //     { path: "trending", Component: ConcertsTrending },
-      //   ],
-      // },
-    ],
+    children: [{ index: true, Component: Home }],
   },
 ]);
