2fe027de5202bb4bcf7dd88cff488f88ce2366d5
keirastanley <keiraestanley@gmail.com>
2025-09-30 01:37:46 +0100

    Revert "feat: update routes to add pages for all lists and individual lists"
    This reverts commit d3902fecadb16c17d2532ed5563b11db9ae50b1a.

--- my-app/src/routes.ts
+++ my-app/src/routes.ts
@@ -1,23 +1,29 @@
 import { createBrowserRouter } from "react-router";
 import { Home } from "./pages/Home";
-import { Layout } from "./components/Layout";
-import { ListsPage } from "./pages/ListsPage";
-import { ListPage } from "./pages/ListPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    Component: Layout,
+    Component: Home,
     children: [
       { index: true, Component: Home },
-      {
-        path: "lists",
-        Component: ListsPage,
-      },
-      {
-        path: "lists/:list_id",
-        Component: ListPage,
-      },
+      // { path: "about", Component: About },
+      // {
+      //   path: "auth",
+      //   Component: AuthLayout,
+      //   children: [
+      //     { path: "login", Component: Login },
+      //     { path: "register", Component: Register },
+      //   ],
+      // },
+      // {
+      //   path: "concerts",
+      //   children: [
+      //     { index: true, Component: ConcertsHome },
+      //     { path: ":city", Component: ConcertsCity },
+      //     { path: "trending", Component: ConcertsTrending },
+      //   ],
+      // },
     ],
   },
 ]);
