9b0f9bf99ec76b39752405d32b97c74be44ac551
keirastanley <keiraestanley@gmail.com>
2025-10-01 14:17:34 +0100

    feat: create MotionLabel component

---
+++ my-app/src/components/CheckboxGroup/MotionLabel.tsx
@@ -0,0 +1,38 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import * as motion from "motion/react-client";
+import { ComponentProps } from "react";
+
+export const MotionLabel = ({
+  children,
+  isSelected,
+  checkboxName,
+}: {
+  isSelected: boolean;
+  checkboxName: string;
+} & ComponentProps<typeof motion.label>) => (
+  <motion.label
+    css={css`
+      padding: 5px 15px;
+      font-size: 12px;
+      text-align: center;
+      border-radius: 15px;
+      border: 1px solid black;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      background-color: ${isSelected ? "grey" : "white"};
+      color: ${isSelected ? "white" : "black"};
+    `}
+    htmlFor={checkboxName}
+    layout
+    transition={{
+      type: "spring",
+      damping: 20,
+      stiffness: 300,
+    }}
+  >
+    {children}
+  </motion.label>
+);
