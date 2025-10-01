e5a3ed05a636d85ae17ec1dc067f6ea9a87a40b4
keirastanley <keiraestanley@gmail.com>
2025-10-01 14:17:18 +0100

    feat: use Overlay on home page

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -2,23 +2,11 @@
 import { css } from "@emotion/react";
 import { useMemo } from "react";
 import { NewRecommendations } from "../sections/NewRecommendations";
-import styled from "@emotion/styled";
 import { useRecommendations } from "../providers/RecommendationsProvider";
 import { sortRecommendationsByDate } from "../utils/utils";
 import { RecommendationsVertical } from "../sections/RecommendationsVertical";
 import { PageWrapper } from "../components/PageWrapper";
-
-const Overlay = styled.div`
-  background-color: rgba(0, 0, 0, 0.5);
-  height: 100vh;
-  width: 100vw;
-  position: fixed;
-  z-index: 2;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-`;
+import { Overlay } from "../components/Overlay";
 
 export const Home = () => {
   const { recommendations, selectedRecommendation } = useRecommendations();
@@ -29,11 +17,10 @@ export const Home = () => {
 
   return (
     <PageWrapper>
-      {selectedRecommendation && <Overlay />}
+      <Overlay show={!!selectedRecommendation} />
       <h1>Welcome to kellaspace</h1>
       <NewRecommendations />
       <div
-        id="here"
         css={css`
           height: 75%;
           max-height: 75%;
