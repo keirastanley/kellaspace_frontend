abde6b2bd3019e1917946dcd610771939f2df2af
keirastanley <keiraestanley@gmail.com>
2025-10-07 02:54:00 +0100

    feat: use ActionProvider on Home page

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -8,10 +8,10 @@ import { RecommendationsVertical } from "../sections/RecommendationsVertical";
 import { PageWrapper } from "../components/PageWrapper";
 import { Overlay } from "../components/Overlay";
 import { HomeAction } from "../interfaces/actions";
+import { ActionsProvider } from "../providers/ActionsProvider";
 
 export const Home = () => {
   const { recommendations, selectedRecommendation } = useRecommendations();
-  const [selectedActions, setSelectedActions] = useState<string[]>([]);
   const [isEditing, setIsEditing] = useState(false);
   const remainingRecommendations = useMemo(
     () => sortRecommendationsByDate(recommendations).slice(6),
@@ -19,27 +19,24 @@ export const Home = () => {
   );
 
   return (
-    <PageWrapper>
-      <Overlay show={!!selectedRecommendation} />
-      <h1>Welcome to kellaspace</h1>
-      <NewRecommendations />
-      <div
-        css={css`
-          flex: 1 1 auto;
-          overflow: hidden;
-        `}
-      >
-        <RecommendationsVertical
-          actions={Object.values(HomeAction)}
-          recommendations={remainingRecommendations}
-          showFilters={selectedActions.includes(HomeAction.Filter)}
-          showSorting={selectedActions.includes(HomeAction.Sort)}
-          selectedActions={selectedActions}
-          setSelectedActions={setSelectedActions}
-          setIsEditing={setIsEditing}
-          isEditing={isEditing}
-        />
-      </div>
-    </PageWrapper>
+    <ActionsProvider actions={Object.values(HomeAction)}>
+      <PageWrapper>
+        <Overlay show={!!selectedRecommendation} />
+        <h1>Welcome to kellaspace</h1>
+        <NewRecommendations />
+        <div
+          css={css`
+            flex: 1 1 auto;
+            overflow: hidden;
+          `}
+        >
+          <RecommendationsVertical
+            recommendations={remainingRecommendations}
+            setIsEditing={setIsEditing}
+            isEditing={isEditing}
+          />
+        </div>
+      </PageWrapper>
+    </ActionsProvider>
   );
 };
