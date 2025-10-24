3cd1d430af06e08b63e9cf98ce98e4e6200148d2
keirastanley <keiraestanley@gmail.com>
2025-10-24 14:01:05 +0100

    feat: improve Layout to handle overflow

--- my-app/src/components/shared/Layout.tsx
+++ my-app/src/components/shared/Layout.tsx
@@ -1,9 +1,33 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import { Outlet } from "react-router";
 import { NavBar } from "./NavBar";
 
 export const Layout = () => (
-  <>
-    <Outlet />
-    <NavBar />
-  </>
+  <div
+    css={css`
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+    `}
+  >
+    <div
+      css={css`
+        flex: 1;
+        overflow-y: auto;
+      `}
+    >
+      <Outlet />
+    </div>
+    <div
+      css={css`
+        position: sticky;
+        bottom: 0;
+        flex-shrink: 0;
+        z-index: 10;
+      `}
+    >
+      <NavBar />
+    </div>
+  </div>
 );
