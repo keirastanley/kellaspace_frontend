fc7f623504fa3b7fb9aec2e06448ea5cbca72d0d
keirastanley <keiraestanley@gmail.com>
2026-03-02 23:09:46 +0000

    feat: Layout improved

--- my-app/src/components/shared/Layout.tsx
+++ my-app/src/components/shared/Layout.tsx
@@ -1,32 +1,29 @@
+/** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import { Outlet } from "react-router";
 import { NavBar } from "./NavBar";
+import { UserHeader } from "./UserHeader";
 
 export const Layout = () => (
   <div
     css={css`
       display: flex;
       flex-direction: column;
-      height: 100%;
+      height: 100vh;
     `}
   >
-    <div
+    <UserHeader />
+    <main
       css={css`
         flex: 1;
-        overflow-y: auto;
+        min-height: 0;
+        overflow: hidden;
+        display: flex;
+        flex-direction: column;
       `}
     >
       <Outlet />
-    </div>
-    <div
-      css={css`
-        position: sticky;
-        bottom: 0;
-        flex-shrink: 0;
-        z-index: 10;
-      `}
-    >
-      <NavBar />
-    </div>
+    </main>
+    <NavBar />
   </div>
 );
