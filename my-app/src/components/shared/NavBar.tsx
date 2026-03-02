5f32e2f8b6407ba43b820fe3495ca24b8fe88a68
keirastanley <keiraestanley@gmail.com>
2026-03-02 23:09:55 +0000

    feat: NavBar improved

--- my-app/src/components/shared/NavBar.tsx
+++ my-app/src/components/shared/NavBar.tsx
@@ -9,20 +9,16 @@ const Nav = styled.nav`
   padding: 6px;
   background-color: white;
   border-top: 1px solid black;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  z-index: 2;
+  flex-shrink: 0;
 `;
 
 const NavLink = styled.a`
   display: flex;
   flex-direction: column;
-  color: black;
-  height: 45px;
-  gap: 4px;
   align-items: center;
+  gap: 4px;
+  height: 45px;
+  color: black;
   text-decoration: none;
   span {
     font-size: 12px;
@@ -38,14 +34,17 @@ export const NavBar = () => (
       <Icons.Home />
       <span>Home</span>
     </NavLink>
+
     <NavLink href={`/${PageRoutes.Lists}`}>
       <Icons.Bookmark />
       <span>Lists</span>
     </NavLink>
+
     <NavLink href={`/${PageRoutes.Activity}`}>
       <Icons.Activity />
       <span>Activity</span>
     </NavLink>
+
     <NavLink href={`/${PageRoutes.CreateRecommendation}`}>
       <Icons.Create />
       <span>Create</span>
